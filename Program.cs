9c4112d8f2723fdd3861b96e6f545af9f9bf47ec
Nicholas McDowell <Nicholas.mcdowell36@gmail.com>
2023-09-15 23:17:48 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TSP_Butler.commands;
+using TSP_Butler.config;
+
+namespace TSP_Butler
+{
+    public sealed class Program
+    {
+        public static DiscordClient Client { get; set; }
+        public static CommandsNextExtension Commands { get; set; }
+
+        static async Task Main(string[] args)
+        {
+            var jsonReader = new JSONReader();
+            await jsonReader.ReadJSON();
+
+            // Bot configuration
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = jsonReader.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            // Apply the config to this DiscordClient
+            Client = new DiscordClient(discordConfig);
+
+            // Default timeout for commands that use interactivity
+            Client.UseInteractivity(new InteractivityConfiguration()
+            {
+                Timeout = TimeSpan.FromMinutes(2)
+            });
+
+            // Task handler ready event
+            Client.Ready += Client_Ready;
+
+            var commandsConfig = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new String[] { jsonReader.prefix },
+                EnableMentionPrefix = true,
+                EnableDms = true,
+                EnableDefaultHelp = false
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            // Register commands
+            Commands.RegisterCommands<TestCommands>();
+
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        
+        }
+
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ commands/TestCommands.cs
@@ -0,0 +1,48 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSP_Butler.commands
+{
+    public class TestCommands : BaseCommandModule
+    {
+        [Command("test")]
+        public async Task MyFirstCommand(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Hello");
+        }
+
+        [Command("interactivity")]
+        public async Task InterTest(CommandContext ctx)
+        {
+            var interactivity = Program.Client.GetInteractivity();
+
+            var messageToRetrieve = await interactivity.WaitForMessageAsync(message => message.Content == "Hello");
+
+            if(messageToRetrieve.Result.Content == "Hello")
+            {
+                await ctx.Channel.SendMessageAsync($"Hello {ctx.User.Username}, I am your factions butler, what can I do for you today?");
+            }
+            
+        }
+
+        [Command("Reaction")]
+        public async Task ReactionTest(CommandContext ctx)
+        {
+            var interactivity = Program.Client.GetInteractivity();
+
+            var messageToReact = await interactivity.WaitForReactionAsync(message => message.Message.Id == 1152464155354337280);
+
+            if (messageToReact.Result.Message.Id == 1152464155354337280)
+            {
+                await ctx.Channel.SendMessageAsync($"{ctx.User.Username} thinks they are a devil");
+            }
+        }
+    }
+}
